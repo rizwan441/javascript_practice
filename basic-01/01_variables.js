07f5dbb092ded69927c330af5822bf40433a6b4f
rizwan441 <78400593+rizwan441@users.noreply.github.com>
2023-10-18 11:06:18 +0000

    data types and outPut

---
+++ basic-01/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 15542; //cant be changed
+
+let accountEmail = "rizwanzafar441@gmail";
+
+var accoutPassword = "12345";
+accountCity = "hatiian"
+
+//    accountId=2;
+
+accountEmail="hc@hc.com";
+accoutPassword=212121;
+accountCity="mzd"
+// console.log(accountEmail);
+// console.log(accountCity);
+// console.log(accountId);
+
+console.table([accountEmail,accountId,accoutPassword,accountCity])
+
+/*
+Preffer Not to Use Var
+Beacuse Of Issue in Block Scope
+
+
+
+
+*/
---
+++ basic-01/02_datatypes.js
@@ -0,0 +1,32 @@
+//  ----Data Types In JavaScript -----------------
+
+"use strict"; // treact all the Js code as a newer version
+
+
+// alert("Hello") // we are using Node Js not browser
+
+console.log(3+3); /// code readibility shuld be high
+console.log("rizwan");
+
+ 
+
+////data types ////
+
+// number =>  20 to power 53
+// BigInt
+// String;
+// Boolean
+// null +> stand alone value
+// undefined:
+
+// symbol => to find uniqness 
+
+
+
+// Objects 
+
+
+console.log(typeof null); // out put is object
+
+
+
