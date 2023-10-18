07f1403d60ee73c8007630f197020517f53d456d
rizwan441 <78400593+rizwan441@users.noreply.github.com>
2023-10-18 12:09:57 +0000

    summery

---
+++ basic-01/04_dataummary.js
@@ -0,0 +1,16 @@
+
+// two types 
+//     premittive and non-premittive
+
+//1 )Primmitive  ==> 7 types 
+    // they are call by value ,not by reference , given to copy
+
+
+
+    //  they are ==>   String,Number,null,Boolean,undefined,Symbol,BigInt
+
+    // Non peimitive (Reference)
+    //   allocated the reference feom the value 
+    // Array,Object,Function
+
+
