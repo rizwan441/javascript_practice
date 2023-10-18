4f88d1a0827811d80bff4fbe8103e26fbc8ae893
rizwan441 <78400593+rizwan441@users.noreply.github.com>
2023-10-18 13:38:05 +0000

    Maths-num

---
+++ basic-01/07_Math.js
@@ -0,0 +1,33 @@
+// const score = new Number( 400)
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(1))
+
+
+// const hundred = 123.987;
+
+// console.log(hundred.toPrecision(3));
+
+// const hund = 100000000;
+// console.log(hund.toLocaleString('en-In'))
+
+
+
+
+// Math ----------///////////
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
