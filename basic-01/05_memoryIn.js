fb7f2bcde9751f22a542d59cec271f9fd863b09d
rizwan441 <78400593+rizwan441@users.noreply.github.com>
2023-10-18 13:12:58 +0000

    string

---
+++ basic-01/05_memoryIn.js
@@ -0,0 +1,10 @@
+
+
+// stack and heap 
+
+// where stack and heap is  used
+
+// for Prenitive (stack is used)
+
+// for Non premitive (Heap is used)
+
---
+++ basic-01/06_string.js
@@ -0,0 +1,31 @@
+const name = 'rizwan';
+const repoConst = 50;
+
+// console.log(name + repoConst + " "
+
+//  string Interpolation
+
+console.log(`The name is ${name} and repo count is ${repoConst}`)
+
+const myStr = new String("Hello Word");
+
+console.log(myStr[0])
+console.log(myStr.length)
+console.log(myStr.charAt(3))
+console.log(myStr.indexOf("r"))
+
+
+const myagain = myStr.substring(0,4)
+console.log(myagain)
+
+
+const anotherstring = myStr.slice(-8,4)
+
+console.log(anotherstring)
+
+const new1 = "       hitesh "
+console.log(new1);
+console.log(new1.trim())
+console.log(new1.split('-'))
+
+
