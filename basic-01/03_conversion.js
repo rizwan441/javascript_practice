d9048c683ed90c0dc9272c659db7d4546b6d50c5
rizwan441 <78400593+rizwan441@users.noreply.github.com>
2023-10-18 11:52:29 +0000

    comperison

---
+++ basic-01/03_conversion.js
@@ -0,0 +1,36 @@
+
+
+
+
+//"33"=> 33
+// "33ab" => NaN
+// true => 1;
+
+// let isLoggedIn = 1;
+// let bool = Boolean(isLoggedIn);
+
+// console.log(bool);
+
+
+
+// -----------------Operations ---------------
+
+//  let val = 3;
+//  let negVal = -val;
+
+//  console.log(negVal);
+
+// console.log("1">0);
+// console.log('02'>1);
+
+console.log(null ==0)
+console.log(null>0)
+console.log(null>=0)
+
+
+
+
+
+
+
+
